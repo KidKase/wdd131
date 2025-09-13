d6a1fde4c208d58f9cd18791338bd2684e9cc7b6
KidKase <kaseekennedy0@gmail.com>
2025-09-13 19:47:04 +0000

    tem

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,17 +1,13 @@
-// Dynamically set the current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Dynamically set last modified date
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+  //  set the current year
+  document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-// Toggle the navigation menu on small screens
-function toggleMenu() {
+  //  set last modified date
+  document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+  // Toggle the navigation menu on small screens
+  function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
-}
+  }
 
-// hamburger button for small screens
-hamButton.addEventListener("", () => {
-	navigation.classList.toggle("open");
-	hamButton.classList.toggle("open");
-});
